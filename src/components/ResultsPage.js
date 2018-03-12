512917beeec652be3dd214b1a7158af723cac6e5
Jelena Gruica <jelena.gruica@gmail.com>
2018-03-11 19:09:54 -0700

    results page header, footer

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -3,6 +3,8 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import Radar from 'react-d3-radar';
 import { Media } from 'react-bootstrap';
+import Header from './Header'
+import Footer from './Footer'
 
 const assessment = gql`
   query assessment($assessmentId: String!) {
@@ -53,94 +55,98 @@ class ResultsPage extends Component {
     if (!this.props.data.loading) {
       const questions = this.props.data.assessment.questions;
       return (
-        <div className='results-page'>
-          <div className='overview'>
-            <div className='svg'>
-              <Radar
-                width={485}
-                height={450}
-                padding={70}
-                domainMax={4}
-                highlighted={null}
-                data={{
-                  variables: questions.map((question, index) => {
-                    return { key: question.id, label: question.text }
-                  }),
-                  sets: [{
-                    key: 'res',
-                    values: questions.reduce((result, question) => {
-                      const selectedChoice = question.choices.find(c => c.isSelected)
-                      console.log(choicePointScore(selectedChoice))
-                      return Object.assign(result, { [question.id]: choicePointScore(selectedChoice) })
-                    }, {})
-                  }]
-                }}
-              />
-            </div>
-
-            <div className='ov-text'>
-              <div>
-              <span className={'circle-score-' + scoreLabel(this.finalScore())}> {scoreLabel(this.finalScore())} </span>
-                <h1> Here is your EnterpriseGrade </h1>
-              
+        <div>
+          <Header />
+          <div className='results-page'>
+            <div className='overview'>
+              <div className='svg'>
+                <Radar
+                  width={485}
+                  height={450}
+                  padding={70}
+                  domainMax={4}
+                  highlighted={null}
+                  data={{
+                    variables: questions.map((question, index) => {
+                      return { key: question.id, label: question.text }
+                    }),
+                    sets: [{
+                      key: 'res',
+                      values: questions.reduce((result, question) => {
+                        const selectedChoice = question.choices.find(c => c.isSelected)
+                        console.log(choicePointScore(selectedChoice))
+                        return Object.assign(result, { [question.id]: choicePointScore(selectedChoice) })
+                      }, {})
+                    }]
+                  }}
+                />
               </div>
 
-              <p className='first'> Awesome! You’re already taking important steps toward becoming EnterpriseReady </p>
-              <p className='second'> Below you'll find the details of your results, as well as a few suggestions for what
-          the next steps you can take to be enterprise ready are. </p>
-            </div>
-          </div>
+              <div className='ov-text'>
+                <div>
+                  <span className={'circle-score-' + scoreLabel(this.finalScore())}> {scoreLabel(this.finalScore())} </span>
+                  <h1> Here is your EnterpriseGrade </h1>
 
-          <div className='container-second'>
-            <h2> Let's take a closer look at your scores </h2>
-            <p> Select a category below to get a more detailed view of your scores, as well as
+                </div>
+
+                <p className='first'> Awesome! You’re already taking important steps toward becoming EnterpriseReady </p>
+                <p className='second'> Below you'll find the details of your results, as well as a few suggestions for what
+          the next steps you can take to be enterprise ready are. </p>
+              </div>
+            </div>
+
+            <div className='container-second'>
+              <h2> Let's take a closer look at your scores </h2>
+              <p> Select a category below to get a more detailed view of your scores, as well as
           next steps on becoming more EnterpriseReady. </p>
 
-            <div>
-              {questions.map((question, index) => {
-                const selectedChoice = question.choices.find(c => c.isSelected)
-                return (
-                  <div className='second-box' key={question.id}>
-                    <div className='box-heading'>{question.text === 'Role-based Access Control' ? 'Role-based AC' :
-                      question.text}</div>
-                    <div className='imgBox'>
-                      <div className='categoryBoxImg'> </div>
-                      <span className={'score-' + scoreLabel(choicePointScore(selectedChoice))}>
-                        {scoreLabel(choicePointScore(selectedChoice))}
-                      </span>
+              <div>
+                {questions.map((question, index) => {
+                  const selectedChoice = question.choices.find(c => c.isSelected)
+                  return (
+                    <div className='second-box' key={question.id}>
+                      <div className='box-heading'>{question.text === 'Role-based Access Control' ? 'Role-based AC' :
+                        question.text}</div>
+                      <div className='imgBox'>
+                        <div className='categoryBoxImg'> </div>
+                        <span className={'score-' + scoreLabel(choicePointScore(selectedChoice))}>
+                          {scoreLabel(choicePointScore(selectedChoice))}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                )
-              })}
+                  )
+                })}
+              </div>
+            </div>
+            <hr />
+            <div className='individual-result'>
+              <Media>
+                {questions.map((question, index) => {
+                  const selectedChoice = question.choices.find(c => c.isSelected)
+                  return (
+                    <div key={question.id}>
+                      <Media.Left>
+                        <div className='categoryImg' />
+                      </Media.Left>
+                      <Media.Body>
+                        <Media.Heading>
+                          <p className='question-text'>
+                            {question.text}
+                            <span className={'circle-score-' + scoreLabel(choicePointScore(selectedChoice))}>{scoreLabel(choicePointScore(selectedChoice))}</span>
+                          </p>
+                        </Media.Heading>
+                        <p className='answer'> Your answer: </p>
+                        <p className='answer'>{selectedChoice.text} </p>
+                        <p className='recommendation'>{selectedChoice.recommendation}</p>
+
+                      </Media.Body>
+                    </div>
+                  )
+                })}
+              </Media>
             </div>
           </div>
-          <hr />
-          <div className='individual-result'>
-            <Media>
-              {questions.map((question, index) => {
-                const selectedChoice = question.choices.find(c => c.isSelected)
-                return (
-                  <div key={question.id}>
-                    <Media.Left>
-                      <div className='categoryImg' />
-                    </Media.Left>
-                    <Media.Body>
-                      <Media.Heading>
-                        <p className='question-text'>
-                          {question.text}
-                          <span className={'circle-score-' + scoreLabel(choicePointScore(selectedChoice))}>{scoreLabel(choicePointScore(selectedChoice))}</span>
-                        </p>
-                      </Media.Heading>
-                      <p className='answer'> Your answer: </p>
-                      <p className='answer'>{selectedChoice.text} </p>
-                      <p className='recommendation'>{selectedChoice.recommendation}</p>
-
-                    </Media.Body>
-                  </div>
-                )
-              })}
-            </Media>
-          </div>
+          <Footer />
         </div>
       )
     } else {
